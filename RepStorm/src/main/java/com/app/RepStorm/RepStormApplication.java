fbd2e3b17f543cb86f87d6c40aa513f21c60bd93
fkauanGIT <kauanuniversity@gmail.com>
2025-04-22 11:27:41 -0300

    feat: initializer project RepStorm

---
+++ RepStorm/src/main/java/com/app/RepStorm/RepStormApplication.java
@@ -0,0 +1,13 @@
+package com.app.RepStorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepStormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepStormApplication.class, args);
+	}
+
+}
---
+++ RepStorm/src/test/java/com/app/RepStorm/RepStormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.RepStorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepStormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
